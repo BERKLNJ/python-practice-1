511658b1cfebf2da263ca33fe35b574e26a40ba4
BERKLNJ <subanovbayel@gmail.com>
2026-03-18 13:22:47 +0500

    assignment1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-customer_name = str(input("Enter your name: "));
-product_name = str(input("Enter product name: "));
-price = float(input("Enter price per unit in KZT: "));
-quantity = int(input("Enter quantity: "));
-subtotal = price * quantity;
-discount = 0.10 * subtotal if subtotal > 5000 else 0;
-total = subtotal - discount;
+customer_name = str(input("Enter your name: "))
+product_name = str(input("Enter product name: "))
+price = float(input("Enter price per unit in KZT: "))
+quantity = int(input("Enter quantity: "))
+subtotal = price * quantity
+discount = 0.10 * subtotal if subtotal > 5000 else 0
+total = subtotal - discount
 print("=" * 30)
 print(" " * 8 + "SHOP RECEIPT")
 print("=" * 30)
